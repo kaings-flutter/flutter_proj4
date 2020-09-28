cb65cc7876d61b6414c0ebb5f5c95d9cae07c97b
willys_tjiawi <willys@huubap.com>
2020-09-28 17:54:07 +0800

    set listener to false

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -10,8 +10,8 @@ class ProductDetailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context).settings.arguments as String;
 
     // since ProductDetailScreen also a child of MyApp widget, we can listen to Products state
-    final productsData = Provider.of<Products>(context);
-    final loadedProduct = productsData.findById(productId);
+    final loadedProduct = Provider.of<Products>(context, listen: false).findById(
+        productId); // set `listen` to false: this widget won't rebuild even there is changes in Products state. When `listen` is set to `false`, it will ONLY listen for ONCE only!! (NOT ACTIVE LISTENER)
 
     return Scaffold(
       appBar: AppBar(
