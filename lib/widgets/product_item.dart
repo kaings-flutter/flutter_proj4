96fd7a0a2e3bb0ddbdb20963e633070f680b0096
willys_tjiawi <willys@huubap.com>
2020-09-28 18:21:48 +0800

    added comment: listen set to false

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,7 +7,13 @@ import '../providers/product.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(
+      context,
+      // if `listen` set to `false`, the listener won't be active.
+      // It ONLY subscribes for first value and stop!
+      // in this case, it is NOT what we want
+      // listen: false,
+    );
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
