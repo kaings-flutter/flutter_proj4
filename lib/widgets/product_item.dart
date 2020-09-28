ce21aca2e682856582c565fe20ad79ce2a74926d
willys_tjiawi <willys@huubap.com>
2020-09-28 23:17:16 +0800

    alternative Provider syntax using `Consumer`

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,48 +7,52 @@ import '../providers/product.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(
-      context,
-      // if `listen` set to `false`, the listener won't be active.
-      // It ONLY subscribes for first value and stop!
-      // in this case, it is NOT what we want
-      // listen: false,
-    );
+    // final product = Provider.of<Product>(
+    //   context,
+    //   // if `listen` set to `false`, the listener won't be active.
+    //   // It ONLY subscribes for first value and stop!
+    //   // in this case, it is NOT what we want
+    //   // listen: false,
+    // );
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-          child: Image.network(product.imageUrl, fit: BoxFit.cover),
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id,
-            );
-          },
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: product.isFavorite
-                ? Icon(Icons.favorite)
-                : Icon(Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
-          ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            color: Theme.of(context).accentColor,
-            onPressed: () {},
-          ),
-        ),
-      ),
-    );
+    // `Consumer<Product>` below is basically similar to
+    // the above `Provider.of<Product>(context)`. BUT, NOT ONLY THAT!
+    // `Consumer` can also be used to only indicate to ONLY partially Re-Build a part of widget
+    return Consumer<Product>(
+        builder: (ctx, product, child) => ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: GridTile(
+                child: GestureDetector(
+                  child: Image.network(product.imageUrl, fit: BoxFit.cover),
+                  onTap: () {
+                    Navigator.of(context).pushNamed(
+                      ProductDetailScreen.routeName,
+                      arguments: product.id,
+                    );
+                  },
+                ),
+                footer: GridTileBar(
+                  backgroundColor: Colors.black87,
+                  leading: IconButton(
+                    icon: product.isFavorite
+                        ? Icon(Icons.favorite)
+                        : Icon(Icons.favorite_border),
+                    color: Theme.of(context).accentColor,
+                    onPressed: () {
+                      product.toggleFavoriteStatus();
+                    },
+                  ),
+                  title: Text(
+                    product.title,
+                    textAlign: TextAlign.center,
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.shopping_cart),
+                    color: Theme.of(context).accentColor,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ));
   }
 }
