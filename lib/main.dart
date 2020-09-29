f6aa768222a28e6edd596d8adb6699173dd6f461
willys_tjiawi <willys@huubap.com>
2020-09-29 15:51:24 +0800

    working with multiple providers

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './providers/products.dart';
+import './providers/cart.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 
@@ -10,18 +11,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      // there're 2 ways to use `ChangeNotifierProvider`:
-      // 1) using `create: (ctx) => Products()`, OR,
-      //    --> use when you instantiate (new instance) a class and provide that to
-      //        ChangeNotifierProvider, we should use this approach! Just like this one!
-      // 2) using value: Products()
-      //    --> use when you use existing instance (you do not create new instance) to
-      //        provide to ChangeNotifierProvider (e.g. just like in ProductsGrid widget)
-      //        usually part of a List or Grid
-
-      // ChangeNotifierProvider will always clean the old data
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        )
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
